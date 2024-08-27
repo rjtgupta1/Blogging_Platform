9f2ee07e173f9117a20ff7e598a4672418b9c6ee
rajat <imrjtgupta@gmail.com>
2024-08-27 11:38:23 +0530

    started working on client side

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <nav className='navbar'>
+        <div className="logo">Blogging Platform</div>
+        <div className="navlinks">
+          <a href="#">Home</a>
+          <a href="#">Create a Blog</a>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Header
