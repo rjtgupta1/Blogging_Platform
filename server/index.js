7eb88e6f3fa3895220abfedb7a070ac641ffe904
rajat <imrjtgupta@gmail.com>
2024-08-27 11:12:52 +0530

    added some dependecies & connected to the database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = process.env.PORT;
 
 app.get('/',(req,res)=>{
     res.status(200).json({msg:"This is working fine"});
 })
 
-app.listen(5000,()=>{
-    console.log("Server is running on port 5000");
-})
+// database & server connection
+try {
+    mongoose.connect(process.env.DB_URI);
+    app.listen(port,()=>{
+        console.log(`Server is running on port ${port}`);
+    })
+} catch (error) {
+    console.log(error);
+}
